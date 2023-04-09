5b21f5c3494aa8b01ecc00f85b0d42bf31c0a5cd
Abhishek Pratap <pratapsinghabhishek112@gmail.com>
2023-04-09 18:42:12 +0530

    model trainer

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 
 from src.Components.data_transformation import DataTransformation
 from src.Components.data_transformation import DataTransformationConfig
+from src.Components.model_trainer import ModelTrainerConfig,Model_Trainer
 
 @dataclass
 class DataIngestionConfig:
@@ -51,7 +52,11 @@ if __name__=="__main__":
     train_data,test_data =obj.initiate_data_ingestion()
 
     data_tranformation = DataTransformation()
-    data_tranformation.initiate_data_transformation(train_data,test_data)
+    train_array,test_array,_=data_tranformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = Model_Trainer()
+    print(modeltrainer.initiate_model_trainer(train_array,test_array))
+
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from src.logger import logging
+from sklearn.metrics import r2_score
 
 
 def save_object(file_path,obj):
@@ -14,6 +15,25 @@ def save_object(file_path,obj):
 
         with open(file_path,"wb") as file_obj:
             dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        logging.info("entering into evalute model")
+        report ={}
+        for i in range(len(list(models))):
+            model= list(models.values())[i]
+            logging.info("fitting the model no.{}".format(i))
+            model.fit(X_train,y_train)
+            logging.info("predicting the model")
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
 
     except Exception as e:
         raise CustomException(e,sys)
